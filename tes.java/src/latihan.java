090e2274488abf3ff0a6144ffd59bb6fbf9ce4d5
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-09-19 22:27:30 +0700

    Update terbaru

---
+++ tes.java/src/latihan.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class latihan {
+    public static void main(String args[]) throws Exception{
+        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Masukkan angka pertama: ");
+        int angka = scanner.nextInt();
+
+        System.out.print("Masukkan angka kedua: ");
+        int angka2 = scanner.nextInt();
+
+        System.out.println("----------------------------");
+        System.out.println("           Pertambahan          ");
+        System.out.println("Ini adalah angka pertama: " + angka);
+        System.out.println("Ini adalah angka kedua: " + angka2);
+
+        int hasil = angka + angka2;
+        int kurang = angka - angka2;
+        int kali = angka * angka2;
+
+        System.out.println("Hasilnya adalah: " + hasil);
+        System.out.println("------- Pengurangan -------");
+        System.out.println("Hasilnya adalah: " + kurang);
+        System.out.println("------- Perkalian -------");
+        System.out.println("Hasilnya adalah: " + kali);
+
+    }
+}
