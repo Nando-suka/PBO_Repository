303c3890a6c1dbbd9d9d0c408ee1c26d4a7495a8
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-09-28 20:44:13 +0700

    Menambahkan file terbaru

---
+++ tes.java/src/belajarDesktop.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class belajarDesktop {
+    public static void main(String[] args) {
+
+        // Membuat frame (jendela)
+        JFrame mywindow = new JFrame();
+
+        mywindow.setTitle("Jendela Program");
+
+        // tentukan ukuran jendela
+        mywindow.setSize(400, 200);
+
+        // tampilkan layer jendela
+        mywindow.setVisible(true);
+  }
+}
+
+
