f5448041471f467566a0cd467ab32d002161e66c
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-10-08 14:31:29 +0700

    Delete OOP_01_Minggu_03 directory

--- OOP_01_Minggu_03/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module OOP_01_Minggu_03 {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires java.desktop;
-	
-	opens sisforumahsakit to javafx.graphics, javafx.fxml;
-}
